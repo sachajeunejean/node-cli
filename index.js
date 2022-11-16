3b9eae5e634fe20bbf1462664600e911d8592144
Sacha <sacha@MacBook-Pro-de-Sacha.local>
2022-11-16 14:15:06 +0100

    commit changes

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ import countryList from "country-list";
 import chalk from "chalk";
 
 async function getCountryInformation(url) {
-	const request = await axios.get(url);
-
-	return request;
+	return await axios.get(url);
 }
 
 if (process.argv.length != 3) {
@@ -16,17 +14,22 @@ if (process.argv.length != 3) {
 
 	const country = process.argv[2];
 	const countryCode = countryList.getCode(country);
-	const currentYear = new Date().getFullYear();
-	const url = `https://date.nager.at/api/v3/publicholidays/${currentYear}/${countryCode}`;
-
-	const response = getCountryInformation(url);
-
-	response.then(datas => {
-		for (let i = 0; i < datas.data.length; i++) {
-			const date = chalk.greenBright(datas.data[i].date);
-			const name = chalk.red(datas.data[i].name);
-			const localName = chalk.cyanBright(datas.data[i].localName);
-			console.log(`${date}: ${name} - aka - ${localName}`);
-		}
-	});
+	
+	if (!countryCode) {
+		console.log("Wrong country name.");
+	} else {
+		const currentYear = new Date().getFullYear();
+		const url = `https://date.nager.at/api/v3/publicholidays/${currentYear}/${countryCode}`;
+		
+		const response = getCountryInformation(url);
+		
+		response.then(datas => {
+			for (let i = 0; i < datas.data.length; i++) {
+				const date = chalk.greenBright(datas.data[i].date);
+				const name = chalk.red(datas.data[i].name);
+				const localName = chalk.cyanBright(datas.data[i].localName);
+				console.log(`${date}: ${name} - aka - ${localName}`);
+			}
+		});
+	}
 }
