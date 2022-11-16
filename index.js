7152cd4bc479c7531ea97ef6b29bc124a47536d6
Sacha <sacha@MacBook-Pro-de-Sacha.local>
2022-11-16 11:46:21 +0100

    commit changes

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
-console.log("Hello, Node.JS!");
+const axios = require('axios');
+
+if (process.argv.length != 3) {
+	console.log("Error: the command need one paramater.");
+	return;
+}
+
+const country = process.argv[2];
+const countryCode = 0;
+const currentYear = new Date().getFullYear();
+const url = `https://date.nager.at/api/v3/publicholidays/${currentYear}/${countryCode}`;
+
+console.log(country);
+console.log(currentYear);
+console.log(url);
